1d7661837cfc11fd9e5fd8e075e62e3810635b9a
Loren Oh <loren.gh.oh@gmail.com>
2019-03-15 15:48:30 -0700

    Gather Theil-Sen slope estiamtes

--- src/get_linear_reg.py
+++ src/get_linear_reg.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import GridSearchCV
 
 def calc_slope(counts):
     # Define grid
-    X_ = np.linspace(5, df.max(), 400)
+    X_ = np.linspace(1, np.max(counts), 100)
 
     # grid = GridSearchCV(
     #     KernelDensity(),
@@ -26,100 +26,29 @@ def calc_slope(counts):
     # grid.fit(df.counts[:, None])
     # print(grid.best_params_)
 
-    counts = np.array(df.counts[df.counts > 5])
+    # TODO: examine the impact of this parameter
+    counts = counts[counts > 1] 
     
-    log_kde = KernelDensity(bandwidth=15).fit(counts[:, None])
+    log_kde = KernelDensity(bandwidth=20).fit(counts[:, None])
     kde = np.log10(np.exp(log_kde.score_samples(X_[:, None])))
-
-    plt.plot(np.log10(X_), kde, '-+')
-
-    # Ridge regression
-    reg_model = lm.RidgeCV()
-    reg_model.fit(np.log10(X_[100:, None]), kde[100:])
-
-    y_reg = reg_model.predict(np.log10(X_)[100:, None])
-    plt.plot(np.log10(X_[100:]), y_reg, '--',
-            label='Linear Regression')
 
     # Theil Sen regression
     ts_model = lm.TheilSenRegressor(n_jobs=16)
     ts_model.fit(np.log10(X_[:, None]), kde)
-    y_ts = ts_model.predict(np.log10(X_)[:, None])
-    plt.plot(np.log10(X_), y_ts, '-', c='k',
-            label='Theil-Sen Regression')
 
-    print(reg_model.coef_, ts_model.coef_)
+    return ts_model.coef_
 
-def plot_kde():
+def main():
     src = '/users/loh/nodeSSD/repos/size_oscillation'
 
-    for time in np.arange(0, 540, 5):
-        #---- Plotting 
-        fig = plt.figure(1, figsize=(5, 4))
-        fig.clf()
-        sns.set_context('paper')
-        sns.set_style('ticks', 
-            {
-                'axes.grid': False,
-                'axes.linewidth': '0.75',
-                'grid.color': '0.75',
-                'grid.linestyle': u':',
-                'legend.frameon': True,
-            })
-        plt.rc('text', usetex=True)
-        plt.rc('font', family='Serif')
+    slopes = []
+    for time in np.arange(0, 540):
+        pq_in = f'{src}/2d_cloud_counts_{time:03d}.pq'
+        df = pq.read_pandas(pq_in).to_pandas()
 
-        ax = plt.subplot(1, 1, 1)
-        df = pq.read_pandas(f'{src}/2d_cloud_counts_{time:03d}.pq').to_pandas()
-
-        # Define grid
-        X_ = np.linspace(5, df.max(), 200)
-
-        # grid = GridSearchCV(
-        #     KernelDensity(),
-        #     {'bandwidth': np.linspace(1, 50, 50)},
-        #     n_jobs=16)
-        # grid.fit(df.counts[:, None])
-        # print(grid.best_params_)
-
-        counts = np.array(df.counts[df.counts > 5])
-        
-        log_kde = KernelDensity(bandwidth=10).fit(counts[:, None])
-        kde = np.log10(np.exp(log_kde.score_samples(X_[:, None])))
-
-        plt.plot(np.log10(X_), kde, '-+')
-
-        # Ridge regression
-        reg_model = lm.RidgeCV()
-        reg_model.fit(np.log10(X_[50:, None]), kde[50:])
-
-        y_reg = reg_model.predict(np.log10(X_)[50:, None])
-        plt.plot(np.log10(X_[50:]), y_reg, '--',
-                label='Linear Regression')
-
-        # Theil Sen regression
-        ts_model = lm.TheilSenRegressor(n_jobs=16)
-        ts_model.fit(np.log10(X_[:, None]), kde)
-        y_ts = ts_model.predict(np.log10(X_)[:, None])
-        plt.plot(np.log10(X_), y_ts, '-', c='k',
-                label='Theil-Sen Regression')
-
-        print(reg_model.coef_, ts_model.coef_)
-
-        # Labels
-        plt.legend()
-        ax.set_xscale('log')
-        plt.ylim(top=-1, bottom=-6)
-
-        plt.xlabel(r'$\log_{10}$ Area')
-        plt.ylabel(r'$\log_{10}$ Normalized Density')
-        
-        plt.tight_layout(pad=0.5)
-        # figfile = '../png/{}_BOMEX_12HR.png'.format(os.path.splitext(__file__)[0])
-        figfile = f'../png/temp/cloud_size_dist_{time:03d}.png'
-        print('\t Writing figure to {}...'.format(figfile))
-        plt.savefig(figfile,bbox_inches='tight', dpi=180, \
-                    facecolor='w', transparent=True)
+        slopes += [calc_slope(df.counts)]
+        df_out = pd.DataFrame(slopes, columns=['slope'])
+        df_out.to_parquet(f'../pq/cloud_size_dist_slope.pq')
 
 if __name__ == '__main__':
-    plot_kde()
+    main()
