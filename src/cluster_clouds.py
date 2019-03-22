45efc649956e98aedbdabd7c9a59f0e947922414
Loren Oh <loh@eoas.ubc.ca>
2019-03-22 16:32:08 -0700

    Cluster cores and clouds at once

--- src/cluster_clouds.py
+++ src/cluster_clouds.py
@@ -18,8 +18,13 @@ def sample_conditional_field(ds):
         ds['QV'][:]/1e3,
         ds['QN'][:]/1e3,
         ds['QP'][:]/1e3)
-    buoy_field = (th_v > np.mean(th_v, axis=(1,2)))
-    return buoy_field & (ds['QN'] > 0)
+        
+    buoy_field = (th_v > np.mean(th_v, axis=(1, 2)))
+
+    cloud_field = (ds['QN'] > 0)
+    core_field = buoy_field & cloud_field
+
+    return core_field, cloud_field
 
 @nb.jit(['i8[:](i4[:], i4)'], nogil=True, nopython=True)
 def count_features(labels, n_features):
@@ -31,31 +36,37 @@ def count_features(labels, n_features):
     return c_
 
 def cluster_clouds():
+    # TODO: read from model_config.json dict
     src = '/Howard16TB/data/loh/BOMEX_12HR/variables'
     dest = '/users/loh/nodeSSD/repos/size_oscillation'
 
     bin_struct = morph.generate_binary_structure(3, 1)
-    # Only label 2D structures 
+    # Label 2D structures 
     bin_struct[0] = 0
     bin_struct[-1] = 0
 
     nc_list = sorted(glob.glob(f'{src}/*.nc'))
     for time, item in enumerate(nc_list):
         with xr.open_dataset(item, autoclose=True) as ds:
-            ds = ds.squeeze('time')
+            try:
+                ds = ds.squeeze('time')
+            except:
+                pass
 
-            c_field = sample_conditional_field(ds)
+            core_field, cloud_field = sample_conditional_field(ds)
+            for item in ['core', 'cloud']:
+                c_field = locals()[f'{item}_field']
 
-            label, n_features = measure.label(c_field, structure=bin_struct)
-            label = label.ravel()
+                label, n_features = measure.label(c_field, structure=bin_struct)
+                label = label.ravel()
 
-        f_counts = count_features(label, n_features)
-        df = pd.DataFrame(f_counts, columns=['counts'])
+                f_counts = count_features(label, n_features)
+                df = pd.DataFrame(f_counts, columns=[f'{item}'])
 
-        file_name = f'{dest}/2d_cloud_counts_{time:03d}.pq'
-        df.to_parquet(file_name)
+                file_name = f'{dest}/2d_{item}_counts_{time:03d}.pq'
+                df.to_parquet(file_name)
 
-        print(f"Written {file_name}")
+                print(f"Written {file_name}")
 
 if __name__ == '__main__':
     cluster_clouds()
