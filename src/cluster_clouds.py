3df4c4d81e4d0d7145b73b71ffcde3c57a9a38a6
Loren Oh <loh@eoas.ubc.ca>
2019-03-25 23:19:34 -0700

    Revert "Code cleanup"
    This reverts commit 76e6f3c5abf1f8eb8ee58c07152061b14912fab3.
    
    Revert accidental code overwrite

--- src/cluster_clouds.py
+++ src/cluster_clouds.py
@@ -11,7 +11,6 @@ import scipy.ndimage.morphology as morph
 
 import var_calcs as vc
 
-# TODO: Both clouds and cores
 def sample_conditional_field(ds):
     th_v = vc.theta_v(
         ds['p'][:]*100,
@@ -19,8 +18,13 @@ def sample_conditional_field(ds):
         ds['QV'][:]/1e3,
         ds['QN'][:]/1e3,
         ds['QP'][:]/1e3)
-    buoy_field = (th_v > np.mean(th_v, axis=(1,2)))
-    return buoy_field & (ds['QN'] > 0)
+        
+    buoy_field = (th_v > np.mean(th_v, axis=(1, 2)))
+
+    cloud_field = (ds['QN'] > 0)
+    core_field = buoy_field & cloud_field
+
+    return core_field, cloud_field
 
 @nb.jit(['i8[:](i4[:], i4)'], nogil=True, nopython=True)
 def count_features(labels, n_features):
@@ -32,11 +36,12 @@ def count_features(labels, n_features):
     return c_
 
 def cluster_clouds():
-    src = '/Howard16TB/data/loh/BOMEX_BOWL/variables'
+    # TODO: read from model_config.json dict
+    src = '/Howard16TB/data/loh/BOMEX_12HR/variables'
     dest = '/users/loh/nodeSSD/repos/size_oscillation'
 
     bin_struct = morph.generate_binary_structure(3, 1)
-    # Only label 2D structures 
+    # Label 2D structures 
     bin_struct[0] = 0
     bin_struct[-1] = 0
 
@@ -48,18 +53,20 @@ def cluster_clouds():
             except:
                 pass
 
-            c_field = sample_conditional_field(ds)
+            core_field, cloud_field = sample_conditional_field(ds)
+            for item in ['core', 'cloud']:
+                c_field = locals()[f'{item}_field']
 
-            label, n_features = measure.label(c_field, structure=bin_struct)
-            label = label.ravel()
+                label, n_features = measure.label(c_field, structure=bin_struct)
+                label = label.ravel()
 
-        f_counts = count_features(label, n_features)
-        df = pd.DataFrame(f_counts, columns=['counts'])
+                f_counts = count_features(label, n_features)
+                df = pd.DataFrame(f_counts, columns=[f'{item}'])
 
-        file_name = f'{dest}/2d_cloud_counts_{time:03d}.pq'
-        df.to_parquet(file_name)
+                file_name = f'{dest}/2d_{item}_counts_{time:03d}.pq'
+                df.to_parquet(file_name)
 
-        print(f"Written {file_name}")
+                print(f"Written {file_name}")
 
 if __name__ == '__main__':
     cluster_clouds()
