be23526c37be4ff7b54f9c097883245bfcdbc39c
Loren Oh <loh@eoas.ubc.ca>
2019-03-28 15:03:17 -0700

    Fix loss function error

--- src/plot_outliers.py
+++ src/plot_outliers.py
@@ -22,11 +22,17 @@ def detect_outliers(regressor, x, y):
     regressor.fit(x[:, None], y)
     y_ts = regressor.predict(x[:, None])
 
-    # Sort by outliers
-    outlier_list = np.argsort(y - y_ts)
+    # Remove first five bins
+    x = np.delete(x, [0, 1, 2, 3, 4])
+    y = np.delete(y, [0, 1, 2, 3, 4])
+    y_ts = np.delete(y_ts, [0, 1, 2, 3, 4])
 
-    # Maximum number of outliers (20%)
-    max_iter = np.int(0.2 * 50)
+    # Maximum number of outliers (10%)
+    max_iter = np.int(0.1 * 50)
+
+    # Sort by outliers
+    outlier_list = np.argsort(np.abs(y - y_ts))[::-1][:max_iter]
+    
     outliers = []
     for i in range(max_iter):
         # Update outlier list
@@ -38,12 +44,13 @@ def detect_outliers(regressor, x, y):
 
         regressor.fit(X_[:, None], Y_)
         corr = regressor.score(X_[:, None], Y_)
+        # Stop at threshold correlation score
         if corr >= 0.975:
             break
 
     return X_, Y_
 
-def plot_kde():
+def main():
     src = '/users/loh/nodeSSD/repos/size_oscillation'
 
     for time in np.arange(0, 540, 5):
@@ -63,18 +70,24 @@ def plot_kde():
         plt.rc('font', family='Serif')
 
         ax = plt.subplot(1, 1, 1)
-        df = pq.read_pandas(f'{src}/2d_core_counts_{time:03d}.pq').to_pandas()
+
+        case = 'BOMEX_BOWL' # Case name 
+        c_type = 'core' # Type of clouds (core or cloud)
+
+        df = f'{src}/{case}_2d_{c_type}_counts_{time:03d}.pq'
+        df = pq.read_pandas(df).to_pandas()
 
         # Define grid
         X_ = np.logspace(0, np.log10(df.max()), 50)
         
-        log_kde = KernelDensity(bandwidth=2).fit(df.counts[:, None])
+        log_kde = KernelDensity(bandwidth=1).fit(df.counts[:, None])
         kde = log_kde.score_samples(X_[:, None]) / np.log(10)
 
         plt.plot(np.log10(X_[:, None]), kde, '-+')
 
         # Filter outliers
-        reg_model = lm.TheilSenRegressor(n_jobs=16)
+        # reg_model = lm.TheilSenRegressor(n_jobs=16)
+        reg_model = lm.HuberRegressor(fit_intercept=False)
         X_, Y_ = detect_outliers(
             reg_model,
             np.log10(X_),
@@ -106,7 +119,7 @@ def plot_kde():
         # Labels
         plt.legend()
         plt.xlim((0, 3.5))
-        plt.ylim((-6, 0))
+        plt.ylim((-6.5, 0))
 
         plt.xlabel(r'$\log_{10}$ Counts')
         plt.ylabel(r'$\log_{10}$ Normalized Density')
@@ -118,4 +131,4 @@ def plot_kde():
                     facecolor='w', transparent=True)
 
 if __name__ == '__main__':
-    plot_kde()
+    main()
