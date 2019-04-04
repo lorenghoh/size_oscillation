32230e90d7632b4086f4e6c0c388deb93cffc3fd
Loren Oh <loh@eoas.ubc.ca>
2019-04-03 17:50:22 -0700

    Update with outlier detection and RANSAC

--- src/plot_linear_reg.py
+++ src/plot_linear_reg.py
@@ -5,38 +5,26 @@ import numba as nb
 import pandas as pd
 import pyarrow.parquet as pq 
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 # Scikit
 from sklearn import linear_model as lm
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import GridSearchCV
 
-import get_linear_reg
+from get_outliers import detect_outliers
+
+from tqdm import tqdm
+
+import lib_plot as plib
+import seaborn as sns
 
 def plot_kde():
     src = '/users/loh/nodeSSD/repos/size_oscillation'
 
-    for time in np.arange(0, 540, 5):
-        #---- Plotting 
-        fig = plt.figure(1, figsize=(5, 4))
-        fig.clf()
-        sns.set_context('paper')
-        sns.set_style('ticks', 
-            {
-                'axes.grid': False,
-                'axes.linewidth': '0.75',
-                'grid.color': '0.75',
-                'grid.linestyle': u':',
-                'legend.frameon': True,
-            })
-        plt.rc('text', usetex=True)
-        plt.rc('font', family='Serif')
+    for time in tqdm(np.arange(0, 540, 5)):
+         #---- Plotting 
+        fig = plib.init_plot((5, 4))
+        ax = fig.add_subplot(111)
 
-        ax = plt.subplot(1, 1, 1)
         df = pq.read_pandas(f'{src}/2d_cloud_counts_{time:03d}.pq').to_pandas()
 
         # Define grid
@@ -45,51 +33,63 @@ def plot_kde():
         log_kde = KernelDensity(bandwidth=1).fit(df.counts[:, None])
         kde = log_kde.score_samples(X_[:, None]) / np.log(10)
 
-        plt.plot(np.log10(X_), kde, '-+')
+        ax.plot(np.log10(X_), kde, '-+')
 
-        # Significance line
-        plt.axhline(
-            np.log10(1/len(df)),
-            c='k', ls='--', lw=0.75)
+        # Filter outliers
+        reg_model = lm.RANSACRegressor()
+        X_, Y_ = detect_outliers(
+            reg_model,
+            np.log10(X_),
+            kde)
 
         # Ridge regression
         reg_model = lm.RidgeCV()
-        reg_model.fit(np.log10(X_[:, None]), kde[:])
+        reg_model.fit(X_[:, None], Y_)
 
-        y_reg = reg_model.predict(np.log10(X_)[:, None])
-        plt.plot(np.log10(X_[:]), y_reg, '--',
+        y_reg = reg_model.predict(X_[:, None])
+        ax.plot(X_, y_reg, '--',
                 label='Linear Regression')
+
+        # RANSAC
+        rs_estimator = lm.RANSACRegressor()
+        rs_estimator.fit(X_[:, None], Y_)
+        y_rs = rs_estimator.estimator_.predict(X_[:, None])
+        ax.plot(X_, y_rs, '-*', c='g',
+                label='RANSAN Regression')
 
         # Theil Sen regression
         ts_model = lm.TheilSenRegressor(n_jobs=16)
-        ts_model.fit(np.log10(X_[:, None]), kde)
-        y_ts = ts_model.predict(np.log10(X_)[:, None])
-        plt.plot(np.log10(X_), y_ts, '-', c='k',
+        ts_model.fit(X_[:, None], Y_)
+        y_ts = ts_model.predict(X_[:, None])
+        ax.plot(X_, y_ts, '-', c='k',
                 label='Theil-Sen Regression')
 
         # Huber regression
         hb_model = lm.HuberRegressor(fit_intercept=False)
-        hb_model.fit(np.log10(X_[:, None]), kde)
-        y_ts = hb_model.predict(np.log10(X_)[:, None])
-        plt.plot(np.log10(X_), y_ts, '-.', c='b',
+        hb_model.fit(X_[:, None], Y_)
+        y_ts = hb_model.predict(X_[:, None])
+        ax.plot(X_, y_ts, '-.', c='b',
                 label='Huber Regression')
 
-        print(reg_model.coef_, ts_model.coef_, hb_model.coef_)
+        # Output slopes
+        tqdm.write(
+            f'{time:3d}, '
+            f'{reg_model.coef_[0]:.3f}, ' 
+            f'{rs_estimator.estimator_.coef_[0]:.3f}, '
+            f'{ts_model.coef_[0]:.3f}, ' 
+            f'{hb_model.coef_[0]:.3f}'
+        )
 
         # Labels
-        plt.legend()
-        plt.xlim((0, 3.5))
-        plt.ylim((-6, 0))
+        ax.legend()
+        ax.set_xlim((0, 3.5))
+        ax.set_ylim((-6, 0))
 
-        plt.xlabel(r'$\log_{10}$ Counts')
-        plt.ylabel(r'$\log_{10}$ Normalized Density')
+        ax.set_xlabel(r'$\log_{10}$ Counts')
+        ax.set_ylabel(r'$\log_{10}$ Normalized Density')
         
-        plt.tight_layout(pad=0.5)
-        # figfile = '../png/{}_BOMEX_12HR.png'.format(os.path.splitext(__file__)[0])
-        figfile = f'../png/temp/cloud_size_dist_{time:03d}.png'
-        print('\t Writing figure to {}...'.format(figfile))
-        plt.savefig(figfile,bbox_inches='tight', dpi=180, \
-                    facecolor='w', transparent=True)
+        file_name = f'../png/temp/cloud_size_dist_{time:03d}.png'
+        plib.save_fig(fig, file_name, False)
 
 if __name__ == '__main__':
     plot_kde()
