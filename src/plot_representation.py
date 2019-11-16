d49945ceebad541d576af4feb570f7fae9cdd565
Loren Oh <loh@eoas.ubc.ca>
2019-11-15 16:06:25 -0800

    Modularize function calls

--- src/plot_representation.py
+++ src/plot_representation.py
@@ -23,30 +23,45 @@ import lib.plot as plib
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def get_counts(df):
-    # return df.cid.value_counts().values
+def get_counts(df, vol=False, return_df=False):
     df['z'] = df.coord // (1536 * 512)
     df_ = df.groupby(['cid', 'z']).size().reset_index(name='counts')
 
-    return df_.counts.values
+    if return_df:
+        return df_
+    elif vol:
+        return df.cid.value_counts().values
+
+    return df_.counts.value_counts().values
 
 def get_rank_rep(df):
-    df['z'] = df.coord // (1536 * 512)
-    df_ = df.groupby(['cid', 'z']).size().reset_index(name='counts')
-    sizes = df_.counts.value_counts().values
-
-    # sizes = df.cid.value_counts().values
-    x, y = np.unique(sizes, return_index=True)
+    counts = get_counts(df)
+    x, y = np.unique(counts, return_index=True)
 
     return x, y
 
-def get_extrema():
-    pass
+def get_extrema(pq_list):
+    maxima = np.zeros(4) # x1, y1, x2, y2
+    def _compswap(maxima, index):
+        pass
+
+    for time, item in enumerate(tqdm(pq_list)):
+        df = pq.read_pandas(item).to_pandas()
+
+        # Rank statistics
+        counts = get_rank_rep(df)
+        maxima[0] = np.max([])
+
+        # Probability density
+        x_grid = np.log10(np.logspace(0, np.log10(np.max(counts)), 35))
+        log_kde = KernelDensity(bandwidth=1).fit(np.log(counts[:, None]))
+        kde = log_kde.score_samples(x_grid[:, None])
+
 
 def plot_scatter(time, df, init=False):
     """
     Plot the size distribution of cloud volumes.
-    
+
     """
     # Select condensed region
     df = df[df.type == 0]
@@ -57,7 +72,8 @@ def plot_scatter(time, df, init=False):
     # Plot 1: Vertical distribution
     ax1 = fig.add_subplot(221)
 
-    ax1.set_xlabel(r'S')
+    ax1.title.set_text('Vertical Size Distribution')
+    ax1.set_xlabel(r'Size')
     ax1.set_ylabel(r'Altitude')
     
     # 3D cloud volumes
@@ -68,13 +84,13 @@ def plot_scatter(time, df, init=False):
 
     # 2D slices, contiguous volumes
     # Always more informative to to is this way
-    df['z'] = df.coord // (1536 * 512)
-    df_ = df.groupby(['cid', 'z']).size().reset_index(name='counts')
+    df_ = get_counts(df, return_df=True)
     ax1.scatter(df_.counts, df_.z, marker='.')
 
     # Plot 2: Rank-size representation
     ax2 = fig.add_subplot(222)
 
+    ax2.title.set_text('Rank-size Representation')
     ax2.set_xlabel(r'$\log_{10}$ R')
     ax2.set_ylabel(r'$\log_{10}$ S')
 
@@ -84,6 +100,7 @@ def plot_scatter(time, df, init=False):
     # Plot 3: Cumulative representation
     ax3 = fig.add_subplot(223)
 
+    ax3.title.set_text('Cumulative Represenation')
     ax3.set_xlabel(r'$\log_{10}$ R')
     ax3.set_ylabel(r'$\log_{10}$ S$_{c}$')
     
@@ -97,13 +114,14 @@ def plot_scatter(time, df, init=False):
     # Plot 4: PDF representation
     ax3 = fig.add_subplot(224)
 
-    ax3.set_xlabel(r'R')
-    ax3.set_ylabel(r'S')
+    ax3.title.set_text('Probability Density Representation')
+    ax3.set_xlabel(r'$\log_{10}$ Cloud Size')
+    ax3.set_ylabel(r'KDE')
 
     x, y = get_rank_rep(df)
     counts = get_counts(df)
 
-    x_grid = np.log10(np.logspace(0, np.log10(np.max(counts)), 50))
+    x_grid = np.log10(np.logspace(0, np.log10(np.max(counts)), 35))
     if init:
         grid = GridSearchCV(
                     KernelDensity(),
@@ -132,6 +150,7 @@ if __name__ == '__main__':
     src = Path("/users/loh/nodeSSD/temp")
 
     pq_list = sorted(src.glob('BOMEX_SWAMP_volume_*.pq'))
+    # get_extrema(pq_list)
     plot_scatter(719, pq.read_pandas(pq_list[600]).to_pandas())
     # for t, item in enumerate(tqdm(pq_list)):
     #     plot_scatter(t, pq.read_pandas(item).to_pandas())
