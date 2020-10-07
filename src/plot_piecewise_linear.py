c7f9f4f02df0002521d6b0cc521738f68a215f23
Loren Oh <loh@eoas.ubc.ca>
2020-10-07 13:44:26 -0700

    Plot decision tree reg

--- src/plot_piecewise_linear.py
+++ src/plot_piecewise_linear.py
@@ -33,10 +33,11 @@ src = Path(config['case']) / 'clusters'
 
 def plot_piecewise_linear(x, y):
     # ---- Plotting
-    fig = plib.init_plot((6, 4))
-    ax = fig.add_subplot(111)
+    fig = plib.init_plot((6, 8))
+    ax = fig.add_subplot(211)
 
-    linreg = slope(x, y, return_reg=True)
+    # Subplot 1: slope
+    linreg = slope(x, y, max_n=3, return_reg=True)
     y_rs = linreg.predict(x[:, None])
 
     print(linreg.coef_)
@@ -45,6 +46,20 @@ def plot_piecewise_linear(x, y):
     ax.plot(x, y_rs, "-", label="RANSAC")
 
     ax.set_xlabel(r"$\log_{10}$ R")
+    ax.set_ylabel(r"$\log_{10}$ S")
+
+    # Subplot 2: decision tree
+    ax = fig.add_subplot(212)
+    dy = np.gradient(y)
+
+    tree = DecisionTreeRegressor(max_leaf_nodes=3)
+    tree.fit(x[:, None], dy)
+    dys_dt = tree.predict(x[:, None]).flatten()
+
+    ax.plot(x, dy, '.')
+    ax.plot(x, dys_dt, '*')
+
+    ax.set_xlabel(r"$\delta$ $\log_{10}$ R")
     ax.set_ylabel(r"$\log_{10}$ S")
 
     file_name = Path(f"{pwd}/png/piecewise_linear_fit.png")
