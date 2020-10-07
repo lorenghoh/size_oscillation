3ba35e7fa6099750fda1c6578280bd9178dd4de3
Loren Oh <loh@eoas.ubc.ca>
2020-10-07 00:07:09 -0700

    Quick tree-based piecewise-linear regression implementation

--- src/reg/slopes.py
+++ src/reg/slopes.py
@@ -5,8 +5,12 @@ from pathlib import Path
 # Scikit-learn
 from sklearn import linear_model as lm
 from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeRegressor
 
-from get_outliers import detect_outliers
+from scipy import optimize
+from scipy import interpolate
+
+from reg.outliers import detect_outliers
 
 import lib.config
 
@@ -15,9 +19,9 @@ config = lib.config.read_config()
 pwd = Path(config['pwd'])
 
 
-def regressor(x, y, model='RidgeCV', remove_outlier=True):
+def regressor(x, y, model='RidgeCV', remove_outlier=False):
     if remove_outlier:
-        x, y, mask = detect_outliers(None, np.log10(x), np.log10(y))
+        x, y, mask = detect_outliers(None, x, y)
 
     if model == 'RidgeCV':
         reg = lm.RidgeCV()
@@ -25,11 +29,25 @@ def regressor(x, y, model='RidgeCV', remove_outlier=True):
         reg = lm.RANSACRegressor()
     elif model == 'TheilSen':
         reg = lm.TheilSenRegressor()
+    elif model == 'Piecewise':
+        tree = DecisionTreeRegressor(max_leaf_nodes=2)
+        tree.fit(x[:, None], np.gradient(y[:None]))
+        dys_dt = tree.predict(x[:, None]).flatten()
+
+        # Point of inflection. We are assuming two piecewise-linear
+        # curves, so the first index will be the point of inflection
+        pt_in = np.unique(dys_dt, return_index=True)[-1][0]
+
+        # Regression
+        linreg = lm.RidgeCV()
+        linreg.fit(x[pt_in:, None], y[pt_in:])
+
+        return linreg.coef_.item
     else:
         raise ValueError("Regression model not found.")
     reg.fit(x[:, None], y)
 
-    return reg.coef_
+    return reg.coef_.item()
 
 
 if __name__ == '__main__':
