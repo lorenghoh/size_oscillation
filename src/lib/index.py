85588c1dcf1b3aaf19dbeb37bccb9d66152180ed
Loren Oh <loh@eoas.ubc.ca>
2021-08-02 01:19:36 -0700

    Add model parameters

--- src/lib/index.py
+++ src/lib/index.py
@@ -1,9 +1,15 @@
+nx = 1728
+ny = 576
+nz = 192
+
+
 def index_to_zyx(index):
-    z = index // (ny*nx)
-    index = index % (ny*nx)
+    z = index // (ny * nx)
+    index = index % (ny * nx)
     y = index // nx
     x = index % nx
     return z, y, x
 
+
 def zyx_to_index(z, y, x):
-    return (ny * nx * z + nx * y + x)
+    return ny * nx * z + nx * y + x
