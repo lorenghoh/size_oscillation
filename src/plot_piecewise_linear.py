94985728245b6e74211365d7ca4add4c996c4fa1
Loren Oh <loh@eoas.ubc.ca>
2021-10-22 14:21:20 -0700

    Fix plot

--- src/plot_piecewise_linear.py
+++ src/plot_piecewise_linear.py
@@ -13,7 +13,8 @@ from scipy import interpolate
 # Set-up local (and temporary) sys.path for import
 # All scripts for calculations and plots need this
 from context import add_path
-add_path(Path('.').resolve())
+
+add_path(Path(".").resolve())
 
 try:
     import lib.plot as pl
@@ -27,47 +28,53 @@ except Exception:
     raise Exception("Issue with dynamic import")
 
 config = lib.config.read_config()
-pwd = Path(config['pwd'])
-src = Path(config['case']) / 'clusters'
+pwd = Path(config["pwd"])
+src = Path(config["case"]) / "clusters"
 
 
 def plot_piecewise_linear(x, y):
     # ---- Plotting
     fig = pl.init_plot((6, 8))
-    ax = fig.add_subplot(211)
 
-    # Subplot 1: slope
+    # Subplot 1: decision tree
+    ax = fig.add_subplot(211)
+    dy = np.gradient(y)
+
+    tree = DecisionTreeRegressor(max_leaf_nodes=2)
+    tree.fit(x[:, None], dy)
+    dys_dt = tree.predict(x[:, None])
+
+    colors = iter(["C0", "C1"])
+
+    ax.plot(x, dy, ".", ms=8)
+    for item in np.unique(dys_dt):
+        mask = dys_dt == item
+        ax.plot(x[mask], dys_dt[mask], "-", color="C1", lw=2)
+        ax.axvspan(x[mask][0], x[mask][-1], color=next(colors), alpha=0.2)
+
+    ax.set_xlabel(r"$\log_{10}$ Cloud Size", fontsize=12)
+    ax.set_ylabel(r"$\delta \log_{10}$ Normalized Density", fontsize=12)
+
+    # Subplot 2: slope
+    ax = fig.add_subplot(212)
+
     linreg = slope(x, y, max_n=2, return_reg=True)
     y_rs = linreg.predict(x[:, None])
 
     print(linreg.coef_)
 
-    ax.plot(x, y, '*')
-    ax.plot(x, y_rs, "-", label="RANSAC")
+    ax.plot(x, y, ".", ms=8)
+    ax.plot(x, y_rs, "-")
 
-    ax.set_xlabel(r"$\log_{10}$ R", fontsize=14)
-    ax.set_ylabel(r"$\log_{10}$ S", fontsize=14)
-
-    # Subplot 2: decision tree
-    ax = fig.add_subplot(212)
-    dy = np.gradient(y)
-
-    tree = DecisionTreeRegressor(max_leaf_nodes=2)
-    tree.fit(x[:, None], dy)
-    dys_dt = tree.predict(x[:, None]).flatten()
-
-    ax.plot(x, dy, '.')
-    ax.plot(x, dys_dt, '*', ms=5)
-
-    ax.set_xlabel(r"$\log_{10}$ R", fontsize=14)
-    ax.set_ylabel(r"$\delta$ $\log_{10}$ S", fontsize=14)
+    ax.set_xlabel(r"$\log_{10}$ Cloud Size", fontsize=12)
+    ax.set_ylabel(r"$\log_{10}$ Normalized Density", fontsize=12)
 
     file_name = Path(f"{pwd}/png/piecewise_linear_fit.png")
     pl.save_fig(fig, file_name)
 
 
 def main():
-    cluster_list = sorted(src.glob('*.pq'))
+    cluster_list = sorted(src.glob("*.pq"))
     cluster = cluster_list[-360]
 
     samples = sample(cluster)
