9123f41b3e1ca50cf051cd64aee48f3b3d31b127
Loren Oh <loh@eoas.ubc.ca>
2021-10-29 01:51:40 -0700

    Add additional tree selection

--- src/reg/slopes.py
+++ src/reg/slopes.py
@@ -19,18 +19,24 @@ config = lib.config.read_config()
 pwd = Path(config["pwd"])
 
 
-def piecewise_linear(x, y, max_n=2, return_reg=False):
-    tree = DecisionTreeRegressor(criterion="friedman_mse", max_leaf_nodes=max_n)
+def piecewise_linear(x, y, max_n=2, return_reg=False, opt="latter"):
+    tree = DecisionTreeRegressor(max_leaf_nodes=max_n)
     tree.fit(x[:, None], np.gradient(y))
     dys_dt = tree.predict(x[:, None]).flatten()
 
     _, ind, c = np.unique(dys_dt, return_index=True, return_counts=True)
-    pt_in = ind[np.argsort(c)][::-1]
-    pt_in = np.append(pt_in, len(dys_dt))
-
-    # Find the longest segment for regression
-    x_seg = x[pt_in[0] : np.min(pt_in[pt_in > pt_in[0]])]
-    y_seg = y[pt_in[0] : np.min(pt_in[pt_in > pt_in[0]])]
+    if opt == "longest":
+        # Find the longest segment for regression
+        pt_in = ind[np.argsort(c)][::-1]
+        pt_in = np.append(pt_in, len(dys_dt))
+        
+        x_seg = x[pt_in[0] : np.min(pt_in[pt_in > pt_in[0]])]
+        y_seg = y[pt_in[0] : np.min(pt_in[pt_in > pt_in[0]])]
+    else:
+        # Always pick the latter segment
+        # Assume 2 max branches
+        x_seg = x[ind[-1]:]
+        y_seg = y[ind[-1]:]
 
     # Regression
     reg = lm.TheilSenRegressor()
